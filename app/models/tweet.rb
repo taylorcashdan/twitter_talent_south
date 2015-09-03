8938564e663e4456df95abdc938be3c4af16cf37
Taylor Cashdan <taylorcashdan@icloud.com>
2015-09-02 20:26:37 -0400

    login function enabled

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,8 @@
 class Tweet < ActiveRecord::Base
 
+	validates :message, presence: true
+	validates :message, length: {maximum: 140, too_long: "A tweet is only 140 max. Everybody knows that!"}
+
 	belongs_to :user
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
 	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+	validates :username, presence: true, uniqueness: true
+
+
 	has_many :tweets
 
 end
